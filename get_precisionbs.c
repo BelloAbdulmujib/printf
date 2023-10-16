78886a8ddf73844672aab9c836d259e11d3d3282
BigZaya <140633226+BigZaya@users.noreply.github.com>
2023-10-16 23:34:45 +0100

    Update and rename get_precision.c to get_precisionbs.c

--- get_precisionbs.c
+++ get_precisionbs.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
+ * get_precision - Calcs the precision
+ * @format: Formatted string in which sto print the arguments
+ * @i: List of arguments to be printed
+ * @list: list of arguments
+ * Return: Prec.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
@@ -39,4 +38,3 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
-
